9f8b26c4042c587dbcd66ef432a426ba9e732f0d
jakobtroitzch002-wq <jakob.troitzch002@gmail.com>
2026-08-30 10:50:10 +0200

    Clean mod initializer before final worldgen phases

--- src/main/java/de/crocodilandy/sky/CrocodilandySkyMod.java
+++ src/main/java/de/crocodilandy/sky/CrocodilandySkyMod.java
@@ -3,11 +3,9 @@ package de.crocodilandy.sky;
 import net.fabricmc.api.ModInitializer;
 
 public final class CrocodilandySkyMod implements ModInitializer {
-
     @Override
     public void onInitialize() {
         SkyWorldgen.register();
-
         System.out.println("Crocodilandy Sky loaded");
     }
 }
