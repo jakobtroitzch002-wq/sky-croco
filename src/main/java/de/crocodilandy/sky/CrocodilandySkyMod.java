3c095482790efe136a67ef4b46e17d4aefcdc38c
jakobtroitzch002-wq <jakob.troitzch002@gmail.com>
2026-09-23 15:43:46 +0200

    Disable extra post-generation structures during worldgen testing

--- src/main/java/de/crocodilandy/sky/CrocodilandySkyMod.java
+++ src/main/java/de/crocodilandy/sky/CrocodilandySkyMod.java
@@ -6,7 +6,6 @@ public final class CrocodilandySkyMod implements ModInitializer {
     @Override
     public void onInitialize() {
         SkyWorldgen.register();
-        SkyStructurePlacer.register();
         System.out.println("Crocodilandy Sky loaded");
     }
 }
